63fa8f8e9fd2925b05c888eb3fbba4838e76c5bd
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-08-11 21:29:33 +0530

    an error .. couldn't solve yet :)

---
+++ README.md
@@ -0,0 +1,17 @@
+** Parcel 
+- Dev build
+- local server
+- HMR - Hot Module Replacement
+- file watching algorithm (written in c++)
+- caching - faster builds
+- image optimisation
+- Minification 
+- Compress
+- consistent hashing
+- code spliting
+- differential bundling - support older browser
+- diagnostic
+- Error handling
+- HTTPs
+
+
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
 const heading = React.createElement(
     "h1",
     { id: "heading" },
     "hello from react"
   );
-const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(heading)
+ const jsxHeading = <h1>It is jsx</h1>
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(jsxHeading)
