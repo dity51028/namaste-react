b63606d94c6f807a4a4483adb55ce3d0b82180a5
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-08-28 14:45:53 +0530

    router added

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-
-
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import Header from './src/components/Header.jsx';
 import Body from './src/components/Body.jsx';
-
-
+import About from './src/components/About.jsx';
+import Contact from './src/components/Contact.jsx';
+import Error from './src/components/Error.jsx';
 
 
 
@@ -13,13 +13,28 @@ const AppALayout = () => {
   return(
     <div className='app'>
       <Header/>
-      <Body/>
-      /**footer */
+      <Body/>   
     </div>
   )
 
 }
 
+const appRoute = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppALayout/>,
+    errorElement:<Error/>
+  },
+  {
+    path:"/about",
+    element:<About/>
+  },
+  {
+    path:"/contact",
+    element:<Contact/>
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppALayout/>)
+root.render(<RouterProvider router={appRoute}/>)
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h2>It is an about page</h2>
+    </div>
+  )
+}
+
+export default About
